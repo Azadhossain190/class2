7abfc04c1224e5c62d09d07c39ed0a1896c6aad4
Azadhossain190 <143415588+Azadhossain190@users.noreply.github.com>
2023-08-29 20:53:19 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+
+<h1>HOME WORK</h1>
+<?php
+
+$storename ='A to Z Store';
+$bag =50;
+$pent =80;
+$shirt =220;
+
+$result =$bag +$pent +$shirt;
+echo "$storename Have $result Unit Product Available!";
+echo '<br>';
+echo '<br>';
+echo '<hr>';
+
+
+$itcentername ='Stack learner';
+$fullstack =35;
+$frontend =20;
+$backend =45;
+
+echo "$itcentername Have....... <br> Fullstack Student $fullstack <br> Frontend Student $frontend <br> And Backend Student $backend <br>";
+
+$result =$fullstack+$frontend+$backend;
+echo "Our $itcentername Institution Consists Of $result Students In Total";
+echo '<br>';
+echo '<br>';
+echo '<br>' ,'<hr>' ;
+
+
+
+// SIMPLE MATH SOLUTION WITH ASSIGNMENT OPERATOR 
+
+$math ='solution';
+$a =10;
+// $a =$a+40;
+$a +=40;
+echo "The $math Is $a";
+echo '<br>';
+echo '<br>';
+$b =100;
+$b -=40;
+echo "The $math Is $b";
+echo '<br>';
+echo '<br>';
+echo '<br>','<hr>';
+
+
+
+$name ='Azad Hossain';
+$gmail ='azad190148@gmail.com';
+$contact ='01614190148';
+$address ='Raozan,Chittagong';
+echo "The Employer Details Is.... <br>Name -- $name <br> Gmail--$gmail<br>Contact--$contact<br>Address--$address";
+echo '<br>';
+echo '<br>';
+echo '<hr>';
+
+
+
+
+
+
+function sum($employername,$firstmonthsell,$secondmonthsell,$threeemonthsell){
+
+    $result=$firstmonthsell + $secondmonthsell + $threeemonthsell;
+    
+    echo "$employername Three Month Sell Is-- $result";
+    echo '<br>';
+    echo '<hr>';
+}
+sum('Azad',200,222,388);
+sum('rahim',170,49,89);
+sum('Diraj',289,166,31);
+echo '<br>';
+                    
+                            // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
